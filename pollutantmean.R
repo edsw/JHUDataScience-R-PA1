c935adc345011dc7ce6def6928f2a96309441e02
Ed Swindelles <ed@swindelles.com>
2014-05-16 10:01:32 -0400

    first commit

---
+++ pollutantmean.R
@@ -0,0 +1,28 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+
+    stopifnot(pollutant == "sulfate" || pollutant == "nitrate")
+    
+    idcount <- length(id)
+    files <- rep(NA, idcount)  
+    
+    for (i in 1:idcount) {
+        files[i] <- paste(directory, "/",
+                          formatC(id[i], width = 3, format = "d", flag = "0"), ".csv", sep = "")
+    }
+    
+    stopifnot(file.exists(files))
+    
+    means <- 0
+    count <- 0
+    for (f in files) {
+        lines <- read.csv(f)
+        data <- lines[pollutant]
+        good <- data[!is.na(data)]
+        if (length(good) > 0) {
+            means <- means + mean(good)
+            count <- count + 1
+        }
+    }
+    
+    means / count
+}
